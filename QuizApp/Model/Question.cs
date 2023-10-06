7ee9c9238f14eac83a4e3f7c7fe2ccc44510481f
Karacina <savkin.andrey03@mail.ru>
2023-10-06 22:21:51 +0300

    Добавьте файлы проекта.

---
+++ QuizApp/Model/Question.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace QuizApp.Model
+{
+    public class Question
+    {
+        public string Text { get; set; }
+        public List<string> Options { get; set; }
+        public List<int> CorrectOptionIndices { get; set; }
+
+        public Question(string text, List<string> options, List<int> correctOptionIndices)
+        {
+            Text = text;
+            Options = options;
+            CorrectOptionIndices = correctOptionIndices;
+        }
+        public Question(string text, List<string> options, int correctOptionIndices)
+        {
+            Text = text;
+            Options = options;
+            CorrectOptionIndices = new List<int> { correctOptionIndices };
+        }
+    }
+    public class Option
+    {
+        public string Text { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
